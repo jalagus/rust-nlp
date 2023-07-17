539d7ee22a1932dbc07d46f09162d6db5b6e07f7
Jarkko Lagus <jalagus@gmail.com>
2023-07-17 22:10:22 +0300

    Add lowrank representation calculation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Mul};
 use flate2::read::GzDecoder;
 use std::fs;
 use std::io::prelude::*;
 use ndarray::{Array1, Array2};
-
+use ndarray_linalg::{TruncatedSvd, TruncatedOrder};
 
 fn load_word2vec_gzip(filename: String) -> HashMap<String, Vec<f32>> {
     let gzip_file = fs::File::open(filename).unwrap();
@@ -47,34 +47,33 @@ fn encode(text: String, embeddings: HashMap<String, Vec<f32>>) -> Array2<f32> {
         }
     }
 
-    let ndarray_2d: Array2<f32> = Array2::from_shape_vec(
+    let document_matrix: Array2<f32> = Array2::from_shape_vec(
         (vectors.len(), vectors[0].len()),
         vectors.iter().flatten().cloned().collect(),
     ).unwrap();
 
-    ndarray_2d
+    document_matrix
 }
 
-fn lr_cov_repr(doc: &Array2<f32>, k: usize) -> (Array2<f32>, Array1<f32>, Array2<f32>) {
-    let repr = ndarray_linalg::TruncatedSvd::new(doc.t().dot(doc), ndarray_linalg::TruncatedOrder::Largest)
+fn lr_cov_repr(doc: &Array2<f32>, k: usize) -> Array2<f32> {
+    let repr = TruncatedSvd::new(doc.t().dot(doc), TruncatedOrder::Largest)
         .decompose(k)
         .unwrap();
 
-    let (u, s, v) = repr.values_vectors();
-    
+    let (u, s, _) = repr.values_vectors(); 
     let s_sqrt: Array1<f32> = s.iter().map(|x| x.sqrt()).collect();
 
-    (u, s, v)
+    // return U * s^(1/2) 
+    s_sqrt.broadcast((u.dim().0, k)).unwrap().mul(&u)
 }
 
 fn main() {
     //load_word2vec_gzip("google-word2vec.bin.gz".to_string());
     let embeddings = load_glove("glove.dev.50d.txt".to_string());
     let doc = encode(String::from("the said with by his from"), embeddings);
+    let repr = lr_cov_repr(&doc, 2);
 
-    let (u, s, v) = lr_cov_repr(&doc, 2);
-
-    println!("Singular values: {:?}", s);
+    println!("{:?}", repr);
     // Do the Kolmgorov thing with matrix SVDs for
     // https://aclanthology.org/2023.findings-acl.426.pdf
 }
